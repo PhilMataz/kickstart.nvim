6d252e3a082b82beb58bc23b897b75a9b306dbf0
Phillip Matambanadzo <hello@philmataz.dev>
2025-05-04 10:26:38 +0100

    feat: move custom plugins to own directory

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,13 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  { 'christoomey/vim-tmux-navigator' },
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = function()
+      require('nvim-autopairs').setup {}
+    end,
+  },
+}
